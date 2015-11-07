23d7f2377d5c2de57ed8c06a90a13cb69cdb6968
Mcdostone <pronoyann@gmail.com>
2015-11-08 00:58:45 +0100

    Bullshit of library

--- JSON2Calendar.js
+++ JSON2Calendar.js
@@ -1,4 +1,4 @@
-var icalendar = require('icalendar');
+var ical = require('ical-generator');
 var config = require('./config/server');
 var path = require('path');
 var crypto = require('crypto');
@@ -6,23 +6,28 @@ var fs = require('fs');
 
 module.exports = function(json, cb) {
 
-	var ical = new icalendar.iCalendar();
-
+	 // Create new Calendar and set optional fields 
+    cal = ical({});
+ 
 	for(i in json) {
 		var tmp  = json[i];
 
-		var event = new icalendar.VEvent(i);
-		event.setSummary(tmp.title);
+		cal.createEvent({
+	    	start: new Date(tmp.startDate),
+    		end: new Date(tmp.endDate),
+    		summary: tmp.title,
+    		location: 'tmp.location',
+		});
+		
+		/*event.setSummary(tmp.title);
 
 		event.setDate(new Date(tmp.startDate), new Date(tmp.endDate));
 		event.setLocation(tmp.location);
 
-		ical.addComponent(event);
+		ical.addComponent(event);*/
 	}
 
-//	ical.validate();
-
-	cb(ical);
+	cb(cal);
 };
 
 module.exports.writer = function(calendar, cb) {
